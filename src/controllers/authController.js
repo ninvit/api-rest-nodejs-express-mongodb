c3212f372c2090a4dd76ccd67b89367241a94cda
ninvit <ninvit@gmail.com>
2020-03-30 16:52:05 -0300

    auth with token working

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const bcrypt = require('bcryptjs');
+const authConfig = require('../config/auth');
+
+const jwt = require('jsonwebtoken');
 
 const User = require('../models/user');
-
 const router = express.Router();
+
 
 router.post('/register', async (req, res) => {
     const { email } = req.body;
@@ -19,6 +23,26 @@ router.post('/register', async (req, res) => {
     } catch (err) {
         return res.status(400).send({ error: 'Registration failed' });
     }
+});
+
+router.post('/authenticate', async (req, res) => {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email }).select('+password');
+
+    if(!user)
+        return res.status(400).send({ error: 'User not found'});
+
+    if(!await bcrypt.compare(password, user.password))
+        return res.status(400).send({ error: 'Invalid password'});
+
+    user.password = undefined;
+
+    const token = jwt.sign({ id: user.id }, authConfig.secret, {
+        expiresIn: 86400,
+    });
+
+    res.send({ user, token });
 });
 
 module.exports = app => app.use('/auth', router);
