0b1023ab68e3233555c41dbd24c00dcdc1d554a7
ninvit <ninvit@gmail.com>
2020-03-31 14:07:57 -0300

    token sendo passado na criação

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -7,6 +7,12 @@ const jwt = require('jsonwebtoken');
 const User = require('../models/user');
 const router = express.Router();
 
+function generateToken(params = {}) {
+    return jwt.sign(params, authConfig.secret, {
+    expiresIn: 86400,
+});
+}
+
 
 router.post('/register', async (req, res) => {
     const { email } = req.body;
@@ -19,7 +25,11 @@ router.post('/register', async (req, res) => {
         const user = await User.create(req.body);
         
         user.password = undefined;
-        return res.send({ user });
+        
+        return res.send({ 
+            user,
+            token: generateToken({ id: user.id }),
+        });
     } catch (err) {
         return res.status(400).send({ error: 'Registration failed' });
     }
@@ -38,11 +48,10 @@ router.post('/authenticate', async (req, res) => {
 
     user.password = undefined;
 
-    const token = jwt.sign({ id: user.id }, authConfig.secret, {
-        expiresIn: 86400,
-    });
-
-    res.send({ user, token });
+    res.send({ 
+        user, 
+        token: generateToken({ id: user.id }),
+     });
 });
 
 module.exports = app => app.use('/auth', router);
